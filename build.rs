ef240fa4c5e85e86c6ff89eecfb055cf1583f97d
陈立兵 <chenlb49@midea.com>
2024-03-21 17:28:27 +0800

    chore: typo fix

--- build.rs
+++ build.rs
@@ -4,8 +4,7 @@ fn main() {
     // Tell cargo to look for shared libraries in the specified directory
     println!("cargo:rustc-link-search=./");
 
-    // Tell cargo to tell rustc to link the system bzip2
-    // shared library.
+    // Tell cargo to tell rustc to link the chdb shared library.
     println!("cargo:rustc-link-lib=chdb");
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
